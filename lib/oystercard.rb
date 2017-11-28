198662d9a1df8a581f735d60c69b31c3a5353a1f
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-28 16:11:10 +0000

    #11: Saving the entry station

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,23 +1,28 @@
+require_relative 'station'
+
 class Oystercard
-  attr_accessor :balance, :in_journey
+  attr_accessor :balance, :entry_station
   MAXIMUM_BALANCE = 90
   FARE = 1
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
   end
   def top_up(deposit)
     fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if deposit + @balance > MAXIMUM_BALANCE
     @balance += deposit
   end
-  def touch_in
+  def touch_in(station)
     fail 'Insufficient funds' if FARE > balance
-    @in_journey = true
+    @entry_station = station.name
   end
   def touch_out
     deduct
-    @in_journey = false
+    @entry_station = nil
+  end
+  def in_journey?
+    !!@entry_station
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,48 +1,60 @@
-require 'oystercard'
+require 'oystercard.rb'
 
 describe Oystercard do
+  let(:station) { double(:my_station, name: "name")}
+  subject(:card) { described_class.new }
 
   it 'initial balance' do
-    expect(subject.balance).to eq 0
+    expect(card.balance).to eq 0
   end
 
     it 'top up success' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+      expect{ card.top_up 1 }.to change{ card.balance }.by 1
     end
 
     it 'error raised if balance limit is exceeded' do
-      subject.top_up(Oystercard::MAXIMUM_BALANCE)
-      expect{ subject.top_up(0.01) }.to raise_error "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
+      card.top_up(Oystercard::MAXIMUM_BALANCE)
+      expect{ card.top_up(0.01) }.to raise_error "Maximum balance of #{Oystercard::MAXIMUM_BALANCE} exceeded"
     end
-  end
+
 
 context 'transit' do
   describe '#in_journey' do
     it 'where am i' do
-      expect(subject).to respond_to(:in_journey)
+      expect( card ).to respond_to(:in_journey?)
     end
   end
   describe '#touch_in' do
     it 'touch in success' do
-      subject.top_up(5)
-      subject.touch_in
-      expect( subject.in_journey ).to eq true
+      card.top_up(5)
+      card.touch_in(station)
+      expect( card ).to be_in_journey
     end
     it 'touch out fail if lack of balance' do
-      expect { subject.touch_in }.to raise_error 'Insufficient funds'
+      expect { card.touch_in(station) }.to raise_error 'Insufficient funds'
+    end
+    it 'remember entry_station upon touch-in' do
+      card.top_up(20)
+      card.touch_in(station)
+      expect( card.entry_station ).to eq station.name
     end
   end
   describe '#touch_out' do
     it 'touch out success' do
-      subject.top_up(5)
-      subject.touch_in
-      subject.touch_out
-      expect( subject.in_journey ).to eq false
+      card.top_up(5)
+      card.touch_in(station)
+      card.touch_out
+      expect( card ).to_not be_in_journey
     end
     it 'deduct fare success' do
-      expect { subject.touch_out }.to change{ subject.balance }.by(-Oystercard::FARE)
+      expect { card.touch_out }.to change{ card.balance }.by(-Oystercard::FARE)
+    end
+    it 'clears station data upon touch-out' do
+      card.top_up(5)
+      card.touch_in(station)
+      card.touch_out
+      expect( card.entry_station ).to eq nil
     end
   end
-
- end
+end
 end
